c360f2e0bc243d22218a5afe1b67549b269144e8
chinimaya <iamchinimaya@gmail.com>
2025-06-12 18:22:24 +0545

    static in template

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'index.html')
 
@@ -23,10 +25,3 @@ def price(request):
 
 def services(request):
     return render(request, 'services.html')
-
-
-
-
-
-
-
